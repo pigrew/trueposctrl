b351dde7f1df09ef2c91c998f9455e06cad9e095
pigrew <pigrew@gmail.com>
2017-09-07 17:07:44 -0400

    Note about GETSCALINGFACTOR value

--- GPSDO_commandset.h
+++ GPSDO_commandset.h
@@ -7,7 +7,7 @@
         // $GETBDELAY [returns board delay, nanoseconds]
         // $GETDELAY [returns cable delay, nanoseconds]
         // $GETPOS [return position ]
-        // $GETSCALEFACTOR [Returns a float, such as 3.742106e-3] 
+        // $GETSCALEFACTOR [Returns a float, such as 3.742106e-3. Value is crystal's gain in Hz/mV.] 
         // $GETVER [returns version info]
         // $KALDBG <0|1> [Enable reporting of Kalman filter parameters]
         // $PPSDBG <0|1> [Enable or disable timing information every second]
@@ -17,7 +17,7 @@
         // $SETPOS <n> <n> <n> [set position to Lat/Long/Elevation_MSL, send value returned by survey]
         // $SURVEY <n> [survey for n hours, default is 8]
         // $TRAINOXCO [Start OXCO Training. This restarts the board ($PROCEED needed), and measures freq
-        //			    change with 500 ADC count]
+        //			    change with 500 ADC count. See result with $GETSCALINGFACTOR.]
         // $UPDATE FLASH [update flash memory settings]
         //
         // Other unknown commands:
